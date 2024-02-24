7fa925d0ba56d9f4cef4744fd4b08f7c4e781f46
Witzevds <witzvand@student.arteveldehs.be>
2024-02-24 18:17:23 +0100

    cleaned up version

--- app/src/lib/HandlebarsHelpers.js
+++ app/src/lib/HandlebarsHelpers.js
@@ -4,17 +4,6 @@
 import Handlebars from "handlebars";
 
 export default {
-  noop: function (options) {
-    return "magic";
-  },
-  bold: function (options) {
-    const content = options.fn(this);
-    //its more safe
-    return new Handlebars.SafeString(`<strong> ${content} </strong>`);
-  },
-  button: function (options) {
-    return new Handlebars.SafeString(`<button>${options.fn(this)}</button>`);
-  },
   hyperlink: function (url, label) {
     return new Handlebars.SafeString(
       `<a href='${url}'><button><p>${label}</p></button></a>`
